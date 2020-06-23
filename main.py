861b42b1ef19a1e377227a7300847b5b2314919c
unknown <scafereren@gmail.com>
2020-06-23 04:01:33 +0300

    computer specs added

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+
+input_computer = input("Are you searching for a laptop? (y/n)").capitalize()
+cimri = input("Enter an item to search at cimri.com: ")
+
+
+if input_computer.capitalize() == 'Y':
+    is_computer = True
+elif input_computer.capitalize() == 'N':
+    is_computer = False
+else: 
+    exit()
+
+
+print(is_computer)
+
+resultList = []
+result = {}
+key = 0
+
 def getComputerSpecs(link):
-    from bs4 import BeautifulSoup
-    import requests
     
+
     soup2 = BeautifulSoup(requests.get(link).content, 'lxml')
-    
+
     itemSpecs = soup2.find('div', {'class': 'specs'})
 
-    screenResolution = itemSpecs.find('span', {'class', 's10v53f3-6 geozbR'}).getText()
-    coreNumber = itemSpecs.find('span', {'class': 's10v53f3-6 geozbR'}).getText()
-    processorFrequency = itemSpecs.find('span', {'class': 's10v53f3-6 geozbR'}).getText()
-    processorModel = itemSpecs.find('span', {'class': 's10v53f3-6 geozbR'}).getText()
+    screenResolution = itemSpecs.find(
+        'span', {'class', 's10v53f3-6 geozbR'}).getText()
+    coreNumber = itemSpecs.find(
+        'span', {'class': 's10v53f3-6 geozbR'}).getText()
+    processorFrequency = itemSpecs.find(
+        'span', {'class': 's10v53f3-6 geozbR'}).getText()
+    processorModel = itemSpecs.find(
+        'span', {'class': 's10v53f3-6 geozbR'}).getText()
     ramGb = itemSpecs.find('span', {'class', 's10v53f3-6 geozbR'}).getText()
     ramMhz = itemSpecs.find('span', {'class': 's10v53f3-6 geozbR'}).getText()
     os = itemSpecs.find('span', {'class': 's10v53f3-6 geozbR'}).getText()
-    panelType = itemSpecs.find('span', {'class': 's10v53f3-6 geozbR'}).getText()
+    panelType = itemSpecs.find(
+        'span', {'class': 's10v53f3-6 geozbR'}).getText()
 
     #priceGraph = soup2.find('svg', {'class': 'rv-xy-plot__inner'})
 
@@ -22,16 +50,8 @@ def getComputerSpecs(link):
 
 
 
+def scrapeCimriLaptop(cimri = cimri):
 
-
-def scrapeCimri(cimri=input("Enter an item to search at cimri.com: ")):
-
-    from bs4 import BeautifulSoup
-    import requests
-
-    resultList = []
-    result = {}
-    key = 0
 
     try:
         pageNumber = int(BeautifulSoup(requests.get("https://www.cimri.com/arama?page=1&sort=rank%2Cdesc&q=" +
@@ -39,7 +59,7 @@ def scrapeCimri(cimri=input("Enter an item to search at cimri.com: ")):
     except:
         pageNumber = 1
 
-    for page in range(1, 3):
+    for page in range(1, pageNumber):
         link = "https://www.cimri.com/arama?page=" + \
             str(page) + "&sort=rank%2Cdesc&q=" + cimri
 
@@ -47,12 +67,39 @@ def scrapeCimri(cimri=input("Enter an item to search at cimri.com: ")):
             'div', {'id': 'cimri-product'})
         for item in items:
             try:
-                links = "https://www.cimri.com" + \
-                    item.find('a', {'class': 'link-detail'})['href']
-                price = item.find(
-                    'a', {'class': 's14oa9nh-0 fFCyge'}).get_text('!').partition('!')[2]
+                links = "https://www.cimri.com" + item.find('a', {'class': 'link-detail'})['href']
+                price = item.find('a', {'class': 's14oa9nh-0 fFCyge'}).get_text('!').partition('!')[2]
                 title = item.find('h3', {'class': 'product-title'})['title']
-                #specs = getComputerSpecs(link)
+                specs = getComputerSpecs(link)
+                result[key] = {"title": title, "price": price,"link": links, "specs": specs}
+                resultList.append(result[key])
+                key += 1
+            except Exception:
+                pass
+
+    return resultList
+
+
+def scrapeCimri(cimri = cimri):
+
+
+
+    try:
+        pageNumber = int(BeautifulSoup(requests.get("https://www.cimri.com/arama?page=1&sort=rank%2Cdesc&q=" +
+                                                    cimri).content, 'lxml').findAll('a', {'class': 's1pk8cwy-2 dSbtQw'})[-1].get_text())
+    except:
+        pageNumber = 1
+
+    for page in range(1, pageNumber):
+        link = "https://www.cimri.com/arama?page=" + str(page) + "&sort=rank%2Cdesc&q=" + cimri
+
+        items = BeautifulSoup(requests.get(link).content, 'lxml').findAll(
+            'div', {'id': 'cimri-product'})
+        for item in items:
+            try:
+                links = "https://www.cimri.com" + item.find('a', {'class': 'link-detail'})['href']
+                price = item.find('a', {'class': 's14oa9nh-0 fFCyge'}).get_text('!').partition('!')[2]
+                title = item.find('h3', {'class': 'product-title'})['title']
                 result[key] = {"title": title, "price": price, "link": links}
                 resultList.append(result[key])
                 key += 1
@@ -62,29 +109,35 @@ def scrapeCimri(cimri=input("Enter an item to search at cimri.com: ")):
     return resultList
 
 
-def excel_writer(res):
-    import pandas as pd
+""" def excel_writer(res):
     
+
     defaultColumnNames = ["Titles", "Prices", "Links"]
-    df= pd.DataFrame(res).T.rename_axis('Link').reset_index()
-    
-    if len(df.columns) == 3: 
+    df = pd.DataFrame(res).T.rename_axis('Link').reset_index()
+
+    if len(df.columns) == 3:
         df.columns = defaultColumnNames
     else:
-        for i in range (len(df.columns) - 3):
+        for i in range(len(df.columns) - 3):
             defaultColumnNames.append("Spec " + str(i))
         df.columns = defaultColumnNames
-    
-    #Writing them to data.xlsx file
+
+    # Writing them to data.xlsx file
     writer = pd.ExcelWriter('data.xlsx', engine='xlsxwriter')
     df.to_excel(writer, sheet_name="Data", index=False)
-    #Setting width of data
-    worksheet = writer.sheets["Data"] 
+    # Setting width of data
+    worksheet = writer.sheets["Data"]
     for idx, col in enumerate(df):
         series = df[col]
-        max_len = max((series.astype(str).map(len).max(),len(str(series.name))))
+        max_len = max((series.astype(str).map(
+            len).max(), len(str(series.name))))
         worksheet.set_column(idx, idx, max_len)  # set column width
     writer.save()
+ """
 
 
-excel_writer(scrapeCimri())
+
+if is_computer:
+    print(scrapeCimriLaptop())
+else:
+    print(scrapeCimri())
